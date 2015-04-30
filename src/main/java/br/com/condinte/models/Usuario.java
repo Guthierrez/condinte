78d7a82c59ac498095d9837e1df8589f8a976aac
Guthierrez <guthi.inf@gmail.com>
2015-04-30 15:29:35 -0300

    Adição de campos a usuarios

--- src/main/java/br/com/condinte/models/Usuario.java
+++ src/main/java/br/com/condinte/models/Usuario.java
@@ -15,15 +15,24 @@ public class Usuario {
 
 	@Id
 	private String login;
-	@Column(nullable=false)
+
+	@Column(nullable = false)
 	private String senha;
-	@Column(unique = true, nullable=false, length=11)
+
+	@Column(unique = true, nullable = false, length = 11)
 	private String cpf;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@Column(unique = true, nullable = false)
+	private String email;
+
 	@ManyToOne
-	@JoinColumn(name="perfil_nome")
+	@JoinColumn(name = "perfil_nome")
 	private PerfilUsuario perfil;
 
-	@NotBlank(message="Digite seu login!")
+	@NotBlank(message = "Digite seu login!")
 	public String getLogin() {
 		return login;
 	}
@@ -31,8 +40,8 @@ public class Usuario {
 	public void setLogin(String login) {
 		this.login = login;
 	}
-	
-	@NotBlank(message="Digite sua senha!")
+
+	@NotBlank(message = "Digite sua senha!")
 	public String getSenha() {
 		return senha;
 	}
@@ -47,6 +56,22 @@ public class Usuario {
 
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	public PerfilUsuario getPerfil() {
