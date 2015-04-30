af20a050352b63e7d96d3a4511209ac1dd38f054
Guthierrez <guthi.inf@gmail.com>
2015-04-30 15:27:22 -0300

    Dao da classe PerfilUsuario

---
+++ src/main/java/br/com/condinte/dao/PerfilUsuarioDao.java
@@ -0,0 +1,8 @@
+package br.com.condinte.dao;
+
+import br.com.condinte.models.PerfilUsuario;
+import br.com.condinte.util.GenericDao;
+
+public class PerfilUsuarioDao extends GenericDao<PerfilUsuario, String> {
+
+}
