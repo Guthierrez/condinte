3481c5fb3cd84aa8b657ecf54c15f78c1d21fba7
Guthierrez <guthi.inf@gmail.com>
2015-04-30 15:29:06 -0300

    Adição de campos a perfis

--- src/main/java/br/com/condinte/models/PerfilUsuario.java
+++ src/main/java/br/com/condinte/models/PerfilUsuario.java
@@ -10,23 +10,37 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="perfis")
+@Table(name = "perfis")
 public class PerfilUsuario {
 	@Id
 	private String nome;
+
+	private String descricao;
+
 	@OneToMany
-	@JoinTable(name="perfis_permissoes", joinColumns={@JoinColumn(name="perfil_nome")}, inverseJoinColumns={@JoinColumn(name="permissao_nome")})
+	@JoinTable(name = "perfis_permissoes", joinColumns = { @JoinColumn(name = "perfil_nome") }, inverseJoinColumns = { @JoinColumn(name = "permissao_nome") })
 	private List<PermissaoUsuario> permissoes;
-	
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
 	public List<PermissaoUsuario> getPermissoes() {
 		return permissoes;
 	}
+
 	public void setPermissoes(List<PermissaoUsuario> permissoes) {
 		this.permissoes = permissoes;
 	}
