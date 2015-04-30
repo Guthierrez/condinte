d4b6df51a6d67475f532ce958eeacd352ae2c988
Guthierrez <guthi.inf@gmail.com>
2015-04-30 15:28:40 -0300

    Criação de instâncias em DaoFactory

--- src/main/java/br/com/condinte/util/DaoFactory.java
+++ src/main/java/br/com/condinte/util/DaoFactory.java
@@ -3,6 +3,8 @@ package br.com.condinte.util;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.condinte.dao.PerfilUsuarioDao;
+import br.com.condinte.dao.PermissaoUsuarioDao;
 import br.com.condinte.dao.UsuarioDao;
 
 public final class DaoFactory {
@@ -11,6 +13,8 @@ public final class DaoFactory {
 	private static EntityManagerFactory entityManagerFactoryInstance;
 	
 	private static UsuarioDao usuarioDao;
+	private static PerfilUsuarioDao perfilUsuarioDao;
+	private static PermissaoUsuarioDao permissaoUsuarioDao;
 	
 	private DaoFactory(){}
 	
@@ -29,4 +33,17 @@ public final class DaoFactory {
 		return usuarioDao;
 	}
 	
+	public static PerfilUsuarioDao PerfilUsuarioDaoInstance() {
+		if(perfilUsuarioDao == null)
+			perfilUsuarioDao = new PerfilUsuarioDao();
+		
+		return perfilUsuarioDao;
+	}
+	
+	public static PermissaoUsuarioDao PermissaoUsuarioDaoInstance() {
+		if(permissaoUsuarioDao == null)
+			permissaoUsuarioDao = new PermissaoUsuarioDao();
+		
+		return permissaoUsuarioDao;
+	}
 }
