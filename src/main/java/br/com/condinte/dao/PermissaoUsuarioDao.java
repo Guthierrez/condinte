7f9147481bfffab392c14a5942b91dc62249fe54
Guthierrez <guthi.inf@gmail.com>
2015-04-30 15:27:36 -0300

    Dao da classe PermissaoUsuario

---
+++ src/main/java/br/com/condinte/dao/PermissaoUsuarioDao.java
@@ -0,0 +1,8 @@
+package br.com.condinte.dao;
+
+import br.com.condinte.models.PermissaoUsuario;
+import br.com.condinte.util.GenericDao;
+
+public class PermissaoUsuarioDao extends GenericDao<PermissaoUsuario, String> {
+
+}
