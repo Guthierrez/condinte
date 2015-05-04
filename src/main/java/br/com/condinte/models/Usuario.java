ff2b1de719d13081e74ccacf1f3635e1b3f4eba9
Guthierrez <guthi.inf@gmail.com>
2015-05-04 10:49:16 -0300

    Correção de imports

--- src/main/java/br/com/condinte/models/Usuario.java
+++ src/main/java/br/com/condinte/models/Usuario.java
@@ -8,12 +8,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.br.CPF;
-
-import com.sun.istack.internal.NotNull;
 
 @Entity
 @Table(name = "usuarios")
