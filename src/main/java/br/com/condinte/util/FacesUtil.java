ff1323134e0e8b02db7d821ae1814c4697fbef15
Guthierrez <guthi.inf@gmail.com>
2015-05-05 11:20:10 -0300

    Classes FacesUtil para simplicar a adição de mensagens na tela

---
+++ src/main/java/br/com/condinte/util/FacesUtil.java
@@ -0,0 +1,12 @@
+package br.com.condinte.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class FacesUtil {
+	public static void addErrorMessage(String message){
+		FacesContext.getCurrentInstance().addMessage(null, 
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+	}
+
+}
